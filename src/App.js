516d5ccd3e4de65da76005b6df4f0c1aec37322e
MakedonskyVladimir <shurmanoff@mail.ru>
2022-12-05 15:44:40 +0300

    add logics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+
+import React, { useState } from "react";
 function App() {
+  
+let [count, setCount] = useState(100)
+let counterPlus = () => {
+  setCount(count++)
+}
   return (
-    <h1>Счетчик</h1>
+
+    <>
+      <h1>Счетчик</h1>
+      <h2>{count}</h2>
+      <button onClick={counterPlus}>+</button>
+      <button onClick={() => setCount(count--)}>-</button>
+    </>
+
   )
 }
 
