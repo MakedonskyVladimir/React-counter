6ce2b11c1fe3bf935475c0a3436f5d7da2a689c0
MakedonskyVladimir <shurmanoff@mail.ru>
2022-12-06 07:45:54 +0300

    added module with styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import cl from './App.module.css'; 
+
 
 const App = () => {
   const [count, setCount] = React.useState(0)
@@ -11,7 +13,7 @@ const App = () => {
   }
   return (
     <>
-      <h3>Counter</h3>
+      <h3 className={cl.title}>Counter</h3>
       <h1>{count}</h1>
       <button onClick={onClickPlus}>Plus</button>
       <button onClick={onClickMinus}>Minus</button>
